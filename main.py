1e177e85dde88296110bd0fb34e29584f151bdba
ShidouAST <itsukashido1003@gmail.com>
2026-01-08 16:57:28 +0800

    Controller Reading input

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import serial
 import time
 import serial.tools.list_ports
+import pygame
 
 looping = 'y'
-try:
-    ser = serial.Serial('COM7', 115200, timeout=1)
-    time.sleep(2)
-except serial.SerialException as e:
-    print(f"Error: {e}")
-    exit()
-  
 
 def receiveData():
     print("Receiving Data")
@@ -18,7 +12,6 @@ def receiveData():
         if ser.in_waiting > 0:
             message = ser.readline().decode('utf-8').strip()
             print(f"Received from ESP32: {message}")
-
 
 def checkPorts():
     ports = list(serial.tools.list_ports.comports())
@@ -29,23 +22,72 @@ def checkPorts():
     else:
         print("No serial ports found.")
 
-def sendData():
+def sendData(data):
     # Entering data for user
-    user_input = input("Enter data: ")
-
+    # user_input = input("Enter data: ")
     try:
-        byte_written = ser.write(user_input.encode()) 
-
+        byte_written = ser.write(f"{data}\n".encode()) 
         if byte_written > 0:
-            print(f"Successfully sent {byte_written} bytes: {user_input}")
+            print(f"Successfully sent {byte_written} bytes: {data}")
         else:
             print("Failed to send data.")
     except Exception as e:
         print(f"Error while sending data: {e}")
-    
 
-while looping == 'y' or looping == 'Y':
-    sendData()
-    looping = input("Do you want to send another? [Y/N] ") 
+def controllerCheck():
+    pygame.init()
+    pygame.joystick.init()
+    # Checking for available controllers
+    joystick_count = pygame.joystick.get_count()
+    if joystick_count == 0:
+        print("Controller Found")
+    else:
+        # Choose from the list
+        joysticks = [pygame.joystick.Joystick(x) for x in range(joystick_count)]
+        print("Lists of Controllers:")
+        count = 0
+
+        for joystick in joysticks:
+            joystick.init()
+            print(f"{count} - {joystick.get_name()}")
+            count += 1
+        chosenJoystick = input("Choose your controller(#): ")
+        joystick = pygame.joystick.Joystick(int(chosenJoystick))
+
+controllerCheck()
+checkPorts()
+
+port = input("Which Port to use (All Caps): ")
+
+try:
+    ser = serial.Serial(port, 115200, timeout=1)
+    time.sleep(2)
+except serial.SerialException as e:
+    print(f"Error: {e}")
+    exit()
+
+running = True
+print("Test the controller")
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        # Button Presses
+        if event.type == pygame.JOYBUTTONDOWN:
+            print(f"Button {event.button} Pressed!")
+            sendData(event.button)
+        
+        if event.type == pygame.JOYAXISMOTION:
+            if abs(event.value) > 0.5: # deadzone
+                print(f"Axis {event.axis} moved to {event.value:.2f}")
+                sendData(f"{event.axis}/{event.value:.2f}")
+
+pygame.quit()
+
+
+# while looping == 'y' or looping == 'Y':
+#     sendData()
+#     looping = input("Do you want to send another? [Y/N] ") 
 
 ser.close() 
